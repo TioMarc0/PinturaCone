fec48e3f385264420b9fb6a00bf170b00bed5589
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-20 10:47:04 -0300

    Commiting

---
+++ src/pinturaCone.java
@@ -0,0 +1,17 @@
+
+public class pinturaCone {
+	
+	private double r;
+	private double z;
+	private double tipo1 = 238.9f;
+	private double tipo2 = 467.98f;
+	private double tipo3 = 758.34f;
+	
+	
+		
+		public pinturaCone(int R, int Z) {
+		
+}
+
+		
+}
---
+++ src/pinturaConeMain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class pinturaConeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ent = new Scanner(System.in);
+		
+		System.out.println("Informe a altura d");
+		
+		
+		
+	}
+
+}
